55b1c456b175143cfb7982ff9fa71d9e7d7c7cde
Irakli Gozalishvili <contact@gozala.io>
2023-12-17 07:37:28 -0800

    feat: implement naive query engine

--- Readme.md
+++ Readme.md
@@ -1,3 +1,65 @@
 # Deductive
 
-Work-in-progress datalog engine for querying in-memory facts.
+Library for querying in-memory facts using [datalog]
+
+## Example
+
+
+```js
+import { Schema, query } from "deductive"
+
+export const demo = (db) => {
+
+  // We will be trying to find movie titles and director names for movies
+  // where Arnold Schwarzenegger casted. We do not a database schema for
+  // writes but we do need schema for queries meaning we want to define
+  // relations between entities and attributes.
+
+  // Since we will look for directors we define an entity which will have
+  // "person/name" attribute.
+  const director = DB.entity({
+    "person/name": Schema.string(),
+  })
+
+  // We also define entity actor that also has "person/name" attribute.
+  const actor = DB.entity({
+    "person/name": Schema.string(),
+  })
+
+  // Finally we define a movie entity that has relationship with actor and
+  // director entities and a "movie/title" attribute.
+
+  const movie = DB.entity({
+    "movie/title": Schema.string(),
+    "movie/director": director,
+    "movie/cast": actor,
+  })
+
+  query(db, {
+    // We want to select matched director name and matched movie titles from db
+    select: {
+      director: director["person/name"],
+      movie: movie["movie/title"],
+    },
+    where: [
+      // where actor is named "Arnold Schwarzenegger"
+      actor.match({ "person/name": "Arnold Schwarzenegger" }),
+      // and where movie had an actor in the cast
+      movie.match({ "movie/cast": actor })
+    ],
+  })
+  // [
+  //   { director: 'James Cameron', movie: 'The Terminator' },
+  //   { director: 'John McTiernan', movie: 'Predator' },
+  //   { director: 'Mark L. Lester', movie: 'Commando' },
+  //   { director: 'James Cameron', movie: 'Terminator 2: Judgment Day' },
+  //   {
+  //     director: 'Jonathan Mostow',
+  //     movie: 'Terminator 3: Rise of the Machines',
+  //   },
+  // ]
+}
+```
+
+
+[datalog]: https://en.wikipedia.org/wiki/Datalog
---
+++ test/lib.spec.js
@@ -0,0 +1,142 @@
+import * as DB from 'deductive'
+import * as proofsDB from './proof-facts.js'
+import * as moviesDB from './movie-facts.js'
+
+/**
+ * @type {import('entail').Suite}
+ */
+export const testDB = {
+  'test capabilities across ucans': async (assert) => {
+    const uploadLink = DB.Schema.string()
+    const storeLink = DB.Schema.string()
+
+    const space = DB.Schema.string()
+    const uploadID = DB.Schema.string()
+    const storeID = DB.Schema.string()
+
+    const result = DB.query(proofsDB, {
+      select: {
+        uploadLink,
+        storeLink,
+        space,
+      },
+      where: [
+        [uploadLink, 'capabilities', uploadID],
+        [uploadID, 'can', 'upload/add'],
+        [uploadID, 'with', space],
+        [storeLink, 'capabilities', storeID],
+        [storeID, 'can', 'store/add'],
+        [storeID, 'with', space],
+      ],
+    })
+
+    assert.deepEqual(result, [
+      {
+        uploadLink: 'bafy...upload',
+        storeLink: 'bafy...store',
+        space: 'did:key:zAlice',
+      },
+    ])
+  },
+  'test query builder': async (assert) => {
+    const query = DB.select({
+      uploadLink: DB.Schema.string(),
+      storeLink: DB.Schema.string(),
+    }).where(({ uploadLink, storeLink }) => {
+      const space = DB.Schema.string()
+      const uploadID = DB.Schema.string()
+      const storeID = DB.Schema.string()
+
+      return [
+        [uploadLink, 'capabilities', uploadID],
+        [uploadID, 'can', 'upload/add'],
+        [uploadID, 'with', space],
+        [storeLink, 'capabilities', storeID],
+        [storeID, 'can', 'store/add'],
+        [storeID, 'with', space],
+      ]
+    })
+
+    assert.deepEqual(query.execute(proofsDB), [
+      {
+        uploadLink: 'bafy...upload',
+        storeLink: 'bafy...store',
+      },
+    ])
+  },
+
+  'test baisc': async (assert) => {
+    const facts = [
+      DB.assert('sally', 'age', 21),
+      DB.assert('fred', 'age', 42),
+      DB.assert('ethel', 'age', 42),
+      DB.assert('fred', 'likes', 'pizza'),
+      DB.assert('sally', 'likes', 'opera'),
+      DB.assert('ethel', 'likes', 'sushi'),
+    ]
+
+    const e = DB.Schema.string()
+
+    assert.deepEqual(
+      DB.query(
+        { facts },
+        {
+          select: { e },
+          where: [[e, 'age', 42]],
+        }
+      ),
+      [{ e: 'fred' }, { e: 'ethel' }]
+    )
+
+    const x = DB.Schema.string()
+    assert.deepEqual(
+      DB.query(
+        { facts },
+        {
+          select: { x },
+          where: [[DB.Schema._, 'likes', x]],
+        }
+      ),
+      [{ x: 'pizza' }, { x: 'opera' }, { x: 'sushi' }]
+    )
+  },
+
+  'sketch pull pattern': (assert) => {
+    const director = DB.entity({
+      'person/name': DB.Schema.string(),
+    })
+
+    const actor = DB.entity({
+      'person/name': DB.Schema.string(),
+    })
+
+    const movie = DB.entity({
+      'movie/title': DB.Schema.string(),
+      'movie/director': director,
+      'movie/cast': actor,
+    })
+
+    assert.deepEqual(
+      DB.query(moviesDB, {
+        select: {
+          director: director['person/name'],
+          movie: movie['movie/title'],
+        },
+        where: [
+          actor.match({ 'person/name': 'Arnold Schwarzenegger' }),
+          movie.match({ 'movie/cast': actor }),
+        ],
+      }),
+      [
+        { director: 'James Cameron', movie: 'The Terminator' },
+        { director: 'John McTiernan', movie: 'Predator' },
+        { director: 'Mark L. Lester', movie: 'Commando' },
+        { director: 'James Cameron', movie: 'Terminator 2: Judgment Day' },
+        {
+          director: 'Jonathan Mostow',
+          movie: 'Terminator 3: Rise of the Machines',
+        },
+      ]
+    )
+  },
+}
